c48a277826e99f257ff4b1e0a528b2a68fde7d33
complover116 <complover116@gmail.com>
2017-03-23 14:59:39 +0300

    Added build number output

--- src/main/java/com/complover116/jst4/JST4.java
+++ src/main/java/com/complover116/jst4/JST4.java
@@ -2,8 +2,13 @@ package com.complover116.jst4;
 
 public class JST4 {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Logger.getInstance().log(Logger.LogType.SYSTEM, "Logger Initialized");
+		if(Versioner.BUILD_NUMBER != -1)
+		Logger.getInstance().log(Logger.LogType.SYSTEM, "Running build "+Versioner.BUILD_NUMBER);
+		else
+		Logger.getInstance().log(Logger.LogType.SYSTEM, "Running a development build");
 	}
 
 }
---
+++ src/main/java/com/complover116/jst4/Versioner.java
@@ -0,0 +1,5 @@
+package com.complover116.jst4;
+
+public class Versioner {
+	public static final int BUILD_NUMBER=-1;
+}
